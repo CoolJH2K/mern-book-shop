eed18c23df7941ebde1ee7424969d90556e75f60
Janna Harmon <harmonjanna00@gmail.com>
2023-06-03 14:51:31 -0400

    Added some code for the mutations.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,38 @@
+// Import gql
+import {gql} from "@apollo/client";
+
+// Export LOGIN_USER mutation
+export const LOGIN_USER = gql`
+    mutation loginUser() {
+        loginUser() {
+
+        }
+    }
+`;
+
+// Export ADD_USER mutation
+export const ADD_USER = gql`
+    mutation addUser() {
+        addUser() {
+
+        }
+    }
+`;
+
+// Export SAVE_BOOK mutation
+export const SAVE_BOOK = gql`
+    mutation saveBook() {
+        saveBook() {
+
+        }
+    }
+`;
+
+// Export REMOVE_BOOK mutation
+export const REMOVE_BOOK = gql`
+    mutation removeBook() {
+        removeBook() {
+
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,7 @@
+// Import gql
+import {gql} from "@apollo/client";
+
+// Export GET_ME query
+export const GET_ME = gql`
+    query me {}
+`
